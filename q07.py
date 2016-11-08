489af5aeb4683b81d091a702c40643eaa6206f63
aipa <gundamgundam777@gmail.com>
2016-11-08 19:32:54 +0900

    [modify] うまくいきませんでした。。。

--- q07.py
+++ q07.py
@@ -38,10 +38,34 @@ def main():
         binary_number_reverse = binary_number[::-1]
         after_reverse = to_decimal_number(binary_number_reverse, 2)
         if date_format == after_reverse:
+            print(binary_number)
             print(day)
 
+def main_ver2():
+    '''
+    取得したい値には規則性がある
+    左4桁が 1001
+    右4桁が 1001
+    である
 
+    というか25桁の真ん中をとった24桁は対象であります。
+    真ん中はの数字は0か1なので、それでループします。
 
+    :return:
+    '''
+    start_date = date(1964, 10, 10)
+    end_date = date(2020, 7, 24)
+    from_left = int(format(int(start_date.strftime('%Y%m%d')), 'b')[4:12], 2)
+    to_left = int(format(int(end_date.strftime('%Y%m%d')), 'b')[4:12], 2)
+    for i in range(from_left, to_left):
+        l = format(i, 'b')
+        r = l[::-1]
+        for j in range(2):
+            value = '1001' + str(l) + str(j) + str(r) + '1001'
+            try:
+                print(datetime.strptime(str(to_decimal_number(value, 2)), '%Y%m%d').date())
+            except:
+                pass
 
 if __name__ == '__main__':
-    main()
+    main_ver2()
